c4bc9a766d7e7b19a464b42dbcf1f9228aeede48
nathaniaria <nathan.iaria7@gmail.com>
2023-03-20 16:57:08 +1100

    banner loading on mount

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,32 +2,54 @@
 import { BellIcon } from '@heroicons/react/solid'
 import { SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+
 
 function Header() {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  },[])
+
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#14141]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
-        <img
-          src="https://rb.gy/ulxxee"
-          width={100}
-          height={100}
-          className="cursor-pointer object-contain"
-        />
+          <img
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+          />
 
 
-        <ul className=" space-x-4 md:flex">
-            <li className="headerLink">Home</li>
-            <li className="headerLink">TV Shows</li>
-            <li className="headerLink">Movies</li>
-            <li className="headerLink">New & Popular</li>
-            <li className="headerLink">My List</li>
-        </ul>
+          <ul className="hidden sm:flex space-x-4">
+              <li className="headerLink">Home</li>
+              <li className="headerLink">TV Shows</li>
+              <li className="headerLink">Movies</li>
+              <li className="headerLink">New & Popular</li>
+              <li className="headerLink">My List</li>
+          </ul>
         </div>
 
 
         <div className='flex items-center space-x-4 text-sm font-light'>
          <SearchIcon className="h-6 w-6 sm:inline"/>   
-          <p className='hidden lg:inline'>Kids</p>
+          <p className='hidden lg:inline-block'>Kids</p>
           <BellIcon className="h-6 w-6"/>
           <Link href="/account">
           <img
